c1aed71441f76046024db4e701deac7f538ea7a5
Pramod <138546178+Pramod0775213344@users.noreply.github.com>
2026-01-06 19:22:39 +0530

    speed test add

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import ClientLayout from "@/components/ClientLayout";
 import Script from "next/script";
 import AdManager from "@/components/AdManager";
 import SpotlightEffect from "@/components/SpotlightEffect";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
